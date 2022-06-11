353f499f087c4a091d0b12e65fbe0892da2a38d6
Pauli <pauli.sairanen@gmail.com>
2022-06-11 17:14:26 +0300

    created view for biden meme

---
+++ src/Views/BidenEsView.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from '@mui/system'
+
+const BidenEsView = () => {
+
+ return (
+   <Box sx={{width: '100%', height: '100%'}}>
+      Ebin Biden should show here :DDDDDD
+   </Box>
+ )
+}
+
+export default BidenEsView
