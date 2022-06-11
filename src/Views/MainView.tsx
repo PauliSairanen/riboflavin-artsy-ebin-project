a419f228ff8694a4f132be0f3958260ba77523d0
Pauli <pauli.sairanen@gmail.com>
2022-06-11 16:39:26 +0300

    added go deeper button to main view

--- src/Views/MainView.tsx
+++ src/Views/MainView.tsx
@@ -1,13 +1,39 @@
 import React from 'react'
 import { Box } from '@mui/system'
+import useWindowDimensions from '../CustomHooks/useWindowDimensions'
+import { Button } from '@mui/material'
 
 const MainView = () => {
+  const [randomX, setRandomX] = React.useState<number>(250)
+  const [randomY, setRandomY] = React.useState<number>(250)
+  const { height, width } = useWindowDimensions();
+
+  const generateRandomX = () => {
+    return Math.floor(Math.random() * width - 100) 
+  }
+  const generateRandomY = () => {
+    return Math.floor(Math.random() * height)
+  }
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setRandomX(generateRandomX())
+      setRandomY(generateRandomY())
+    }, 4000)
+  }, [randomX, randomY])
+
  return (
    <Box sx={{width: '100%', height: '100%'}}>
      <video  muted autoPlay loop width='100%' style={{objectFit: 'cover', width: '100vw', height: '100vh', position: 'fixed'}}>
        <source src='videos/static.mp4'/>
      </video>
     
+     <Box sx={{ position: 'relative', top: `${randomY}px`, left: `${randomX}px`, }}>
+       <Button sx={{fontSize: '50px', color: 'white', fontWeight: 'bold'}}>
+       Go Deeper?
+       </Button>
+       
+       </Box>
    </Box>
  )
 }
