29add24641007ff0adf59408fd033fd8aca6de01
Pauli <pauli.sairanen@gmail.com>
2022-06-11 17:14:45 +0300

    navigating to new view from mainview

--- src/Views/MainView.tsx
+++ src/Views/MainView.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { Box } from '@mui/system'
 import useWindowDimensions from '../CustomHooks/useWindowDimensions'
 import { Button } from '@mui/material'
+import {useNavigate} from 'react-router-dom'
 
 const MainView = () => {
   const [randomX, setRandomX] = React.useState<number>(250)
   const [randomY, setRandomY] = React.useState<number>(250)
   const { height, width } = useWindowDimensions();
+  const navigate = useNavigate()
+
 
   const generateRandomX = () => {
     return Math.floor(Math.random() * width - 100) 
@@ -22,6 +25,8 @@ const MainView = () => {
     }, 4000)
   }, [randomX, randomY])
 
+  const handleClick = () => navigate('EbinBiden')
+
  return (
    <Box sx={{width: '100%', height: '100%'}}>
      <video  muted autoPlay loop width='100%' style={{objectFit: 'cover', width: '100vw', height: '100vh', position: 'fixed'}}>
@@ -29,7 +34,7 @@ const MainView = () => {
      </video>
     
      <Box sx={{ position: 'relative', top: `${randomY}px`, left: `${randomX}px`, }}>
-       <Button sx={{fontSize: '50px', color: 'white', fontWeight: 'bold'}}>
+       <Button sx={{fontSize: '50px', color: 'white', fontWeight: 'bold'}} onClick={handleClick}>
        Go Deeper?
        </Button>
        
