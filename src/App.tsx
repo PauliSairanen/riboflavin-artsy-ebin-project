a8e7e3acd387a21f55631307d57b03f31cb5e45a
Pauli <pauli.sairanen@gmail.com>
2022-06-11 17:14:13 +0300

    Created routing for application

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import MainView from './Views/MainView'
+import BidenEsView from './Views/BidenEsView';
+import {BrowserRouter, Routes,  Route} from 'react-router-dom'
 
 function App() {
   return (
-      <MainView/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<MainView/>} />
+      <Route path='/ebinBiden' element={<BidenEsView/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
